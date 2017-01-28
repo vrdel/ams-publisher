4966690b38df3fd3b84576b70b74ae2bd001f9fa
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-01-28 23:17:14 +0100

    refactorings and unlock on publish fail

--- pymod/core.py
+++ pymod/core.py
@@ -13,9 +13,11 @@ class Run(object):
         self.log = kwargs['log']
         self.conf = kwargs['conf']
         self.ev = kwargs['ev']
-        self.inmem_q = deque()
-        kwargs.update({'inmem_q': self.inmem_q})
+
+        self.inmemq = set()
+        kwargs.update({'inmem_q': self.inmemq})
         self.publisher = Publish(*args, **kwargs)
+
         self._run()
 
     def _cleanup(self):
@@ -23,39 +25,48 @@ class Run(object):
 
     def _run(self):
         self.nmsgs_consumed, self.sess_consumed = 0, 0
-        self.mq = DQS(path=self.conf['queue'])
+        self.dirq = DQS(path=self.conf['queue'])
+
         while True:
             if self.ev['term'].isSet():
                 self.ev['term'].clear()
                 self.cleanup()
 
-            if self.consume_dirq(100):
+            if self.consume_dirq_msgs(100):
                 if self.publisher.write():
-                    self.remove_queue_msg()
+                    self.remove_dirq_msgs()
+                else:
+                    self.unlock_dirq_msgs()
+
             time.sleep(0.5)
 
-    def consume_dirq(self, num=0):
+    def consume_dirq_msgs(self, num=0):
         try:
-            for name in self.mq:
-                if self.mq.lock(name):
-                    self.inmem_q.append((name, self.mq.get_message(name)))
+            for name in self.dirq:
+                if self.dirq.lock(name):
+                    self.inmemq.append((name, self.dirq.get_message(name)))
                     self.nmsgs_consumed += 1
                     self.sess_consumed += 1
                     if num and self.sess_consumed == num:
                        self.sess_consumed = 0
                        return True
             else:
-                self.log.info('{0} empty'.format(self.mq.path))
+                self.log.info('{0} empty'.format(self.dirq.path))
 
         except Exception as e:
             self.log.error(e)
 
         return False
 
-    def remove_queue_msg(self):
-        for m in self.inmem_q:
-            self.mq.remove(m[0])
-        self.inmem_q.clear()
+    def unlock_dirq_msgs(self):
+        for m in self.inmemq:
+            self.dirq.unlock(m[0])
+        self.inmemq.clear()
+
+    def remove_dirq_msgs(self):
+        for m in self.inmemq:
+            self.dirq.remove(m[0])
+        self.inmemq.clear()
 
 
 class Publish(Run):
@@ -65,7 +76,11 @@ class Publish(Run):
             exec code
 
     def write(self):
-        with open('/root/msgs_file', 'a') as fp:
-            fp.writelines(['{0}\n'.format(str(m[1])) for m in self.inmem_q])
-        return True
+        try:
+            with open('/root/msgs_file', 'a') as fp:
+                fp.writelines(['{0}\n'.format(str(m[1])) for m in self.inmemq])
+                return True
+
+        except Exception as e:
+            return False
 
