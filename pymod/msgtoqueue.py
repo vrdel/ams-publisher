8132ff59cd123d3b67276b5bb5c99539042c92ee
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-02-13 11:55:38 +0100

    handle msgs with paired services

--- pymod/msgtoqueue.py
+++ pymod/msgtoqueue.py
@@ -13,14 +13,34 @@ import pwd
 import sys
 
 conf = '/etc/argo-nagios-ams-publisher/ams-publisher.conf'
+logfile = '/var/log/argo-nagios-ams-publisher/ams-publisher.log'
 
 def seteuser(user):
     os.setegid(user.pw_gid)
     os.seteuid(user.pw_uid)
 
+def build_msg(args, *headers):
+    msg = Message()
+    msg.header = dict()
+    msg.body = str()
+
+    timestamp, service, hostname, metric, status = headers
+
+    msg.header.update({'timestamp': timestamp})
+    msg.header.update({'service': service})
+    msg.header.update({'hostname': hostname})
+    msg.header.update({'metric': metric})
+    msg.header.update({'status': status})
+
+    for bs in ['summary', 'message', 'vofqan', 'voname', 'roc']:
+        code = "msg.body += '%s: ' + args.%s + '\\n' if args.%s else ''" % (bs, bs, bs)
+        exec code
+
+    return msg
+
 def main():
     parser = argparse.ArgumentParser()
-    lobj = log.Logger(sys.argv[0])
+    lobj = log.Logger(sys.argv[0], logfile)
     logger = lobj.get()
     confopts = config.parse_config(logger)
 
@@ -45,23 +65,20 @@ def main():
     seteuser(pwd.getpwnam(confopts['general']['runasuser']))
 
     try:
-        msg = Message()
-        msg.header = dict()
-        msg.body = str()
-
-        msg.header.update({'timestamp': args.timestamp})
-        msg.header.update({'service': args.service})
-        msg.header.update({'hostname': args.hostname})
-        msg.header.update({'metric': args.metric})
-        msg.header.update({'status': args.status})
-
-        for bs in ['summary', 'message', 'vofqan', 'voname', 'roc']:
-            code = "msg.body += '%s: ' + args.%s + '\\n' if args.%s else ''" % (bs, bs, bs)
-            exec code
-
         granularity = config.get_queue_granul(args.queue)
         mq = DQS(path=args.queue, granularity=granularity)
-        mq.add_message(msg)
+
+        if ',' in args.service:
+            services = args.service.split(',')
+            services = [s.strip() for s in services]
+            for service in services:
+                msg = build_msg(args, args.timestamp, service, args.hostname, \
+                                args.metric, args.status)
+                mq.add_message(msg)
+        else:
+            msg = build_msg(args, args.timestamp, args.service, args.hostname, \
+                            args.metric, args.status)
+            mq.add_message(msg)
 
     except MessageError as e:
         logger.error('Error constructing message - %s', repr(e))
@@ -69,5 +86,3 @@ def main():
     except (OSError, IOError) as e:
         logger.error(e)
         raise SystemExit(1)
-
-main()
