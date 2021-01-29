ef2385e021ed7f9369528f455ea002de8a2b1595
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-29 07:46:48 +0100

    fixed logger init for delivery cache tools

--- pymod/alarmtoqueue.py
+++ pymod/alarmtoqueue.py
@@ -14,8 +14,8 @@ import pwd
 import pytz
 import sys
 
-conf = '/etc/argo-nagios-ams-publisher/ams-publisher.conf'
-logfile = '/var/log/argo-nagios-ams-publisher/ams-publisher.log'
+
+CONF = '/etc/argo-nagios-ams-publisher/ams-publisher.conf'
 
 
 def seteuser(user):
@@ -46,7 +46,7 @@ def build_msg(args, *headers):
 
 def main():
     parser = argparse.ArgumentParser()
-    lobj = log.Logger(sys.argv[0], logfile)
+    lobj = log.Logger(sys.argv[0])
     logger = lobj.get()
     confopts = config.parse_config(logger)
     nagioshost = confopts['general']['host']
