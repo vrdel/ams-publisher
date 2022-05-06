9deec89dfe830d973220706051a20b042b022e77
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-05-06 08:55:14 +0200

    refine setup.py for two source builds

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 from distutils.core import setup
-import platform
+from distutils.command.build_py import build_py as _build_py
+import shutil
+import os
+
 
 NAME = 'ams-publisher'
 
-def is_c7():
-    dist = platform.dist()
-    for e in dist:
-        if e.startswith('7'):
-            return True
-    return False
+
+class sensu_build(_build_py):
+    description='Bridge from Sensu to the ARGO Messaging system',
+
+    def __init__(self, args):
+        super(_build_py, self).__init__(args)
+        shutil.copy('init/ams-publisher-nagios.service', 'init/ams-publisher.service')
+        self.run_command('build')
+        self.run_command('sdist')
+        os.remove('init/ams-publisher.service')
+
+
+class nagios_build(_build_py):
+    description='Bridge from Nagios to the ARGO Messaging system',
+
+    def __init__(self, args):
+        super(_build_py, self).__init__(args)
+        shutil.copy('init/ams-publisher-nagios.service', 'init/ams-publisher.service')
+        self.run_command('build')
+        self.run_command('sdist')
+        os.remove('init/ams-publisher.service')
 
 
 def get_ver():
@@ -28,13 +46,13 @@ setup(
     author='SRCE',
     author_email='dvrcic@srce.hr',
     license='ASL 2.0',
-    description='Bridge from Nagios to the ARGO Messaging system',
     long_description='Bridge from Nagios to the ARGO Messaging system',
     url='https://github.com/ARGOeu/argo-nagios-ams-publisher',
     package_dir={'ams_publisher': 'pymod/'},
     packages=['ams_publisher'],
-    data_files=[('/etc/ams-publisher/', ['config/ams-publisher.conf', 'config/metric_data.avsc']),
-                ('/usr/lib/systemd/system/', ['init/ams-publisher.service']) if is_c7() else \
-                ('/etc/init.d/', ['init/ams-publisher'])],
     scripts=['bin/ams-alarm-to-queue', 'bin/ams-metric-to-queue',
-             'bin/ams-publisherd'])
+             'bin/ams-publisherd'],
+    cmdclass={
+        'sensu': sensu_build, 'nagios': nagios_build
+    }
+)
